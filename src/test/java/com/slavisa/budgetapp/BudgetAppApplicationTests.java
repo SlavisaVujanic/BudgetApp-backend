0f884b3bd856c69eb396d3344c0cbebfbdaee66e
SlavisaVujanic <svujaniic@gmail.com>
2025-09-27 00:35:40 +0200

    Testing action

--- src/test/java/com/slavisa/budgetapp/BudgetAppApplicationTests.java
+++ src/test/java/com/slavisa/budgetapp/BudgetAppApplicationTests.java
@@ -2,12 +2,14 @@ package com.slavisa.budgetapp;
 
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 //@SpringBootTest
 class BudgetAppApplicationTests {
 
 	@Test
 	void contextLoads() {
-        //
+        assertTrue(true);
 	}
 
 }
