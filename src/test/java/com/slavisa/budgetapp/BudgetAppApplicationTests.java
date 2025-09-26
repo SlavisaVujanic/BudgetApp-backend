e0645a4bfb4c8d7a5464ac236be84d9f459c7e8d
SlavisaVujanic <svujaniic@gmail.com>
2025-09-26 16:02:30 +0200

    Add workflow

--- src/test/java/com/slavisa/budgetapp/BudgetAppApplicationTests.java
+++ src/test/java/com/slavisa/budgetapp/BudgetAppApplicationTests.java
@@ -1,9 +1,8 @@
 package com.slavisa.budgetapp;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-
+//@SpringBootTest
 class BudgetAppApplicationTests {
 
 	@Test
