ac995699cc2946cd6c5e969747a559ba07dfda61
SlavisaVujanic <svujaniic@gmail.com>
2025-09-26 15:09:26 +0200

    Add workflow

--- src/test/java/com/slavisa/budgetapp/BudgetAppApplicationTests.java
+++ src/test/java/com/slavisa/budgetapp/BudgetAppApplicationTests.java
@@ -1,13 +1,9 @@
 package com.slavisa.budgetapp;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.NONE)
-@EnableAutoConfiguration(exclude = {DataSourceAutoConfiguration.class, HibernateJpaAutoConfiguration.class})
+@SpringBootTest
 class BudgetAppApplicationTests {
 
 	@Test
